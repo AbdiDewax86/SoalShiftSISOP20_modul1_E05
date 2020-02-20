57e7f236d2f569a9baf6e2bd13b8022587c5fd6a
AbdiDewax86 <abdidewax86@gmail.com>
2020-02-21 02:08:30 +0700

    File Soal 1

---
+++ soal1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+awk -f SM1soal1a.awk Sample-Superstore.tsv | sort -g | head -n1 | awk 'BEGIN{print "Region dengan profit terendah:"} {print $2}' # soal 1a
+# awk -f SM1soal1a.awk Sample-Superstore.tsv | sort -g | head -n1 | awk '{print $1}' > soal1a.txt # inputan soal 1b
+
+awk -f SM1soal1b.awk Sample-Superstore.tsv | sort -g | head -n2 | awk 'BEGIN{print "State dengan profit terendah:"} {print $2}' # soal 1b
+
+awk -f SM1soal1cCombined.awk Sample-Superstore.tsv | sort -g | head -n10 | awk 'BEGIN{FS=";"; print "Produk dengan profit terendah:"} {print $2}' # soal 1c
